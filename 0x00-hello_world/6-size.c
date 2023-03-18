861f1f5b469bc8421edf748fc431da8a34f36cb1
bouchra <bouchraabdelmouktadir1@gmail.com@example.com>
2023-03-17 17:49:59 -0700

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
- *
- * Description: using sizeof to print the size of various types.
- *
  * Return: Always 0 (Success)
+ */
 
 int main(void)
-{
-printf("Size of a char: %lu byte(s)\n",sizeof(char));
-printf("Size of an int: %lu byte(s)\n",sizeof(int));
-printf("Size of a long int: %lu byte(s)\n",sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n",sizeof(long long int));
-printf("Size of a float: %lu byte(s)\n",sizeof(float));
-return (0);
 
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
 }
