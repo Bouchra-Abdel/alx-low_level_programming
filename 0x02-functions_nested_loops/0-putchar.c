757ea62f20b8f6f86193a122974ab546e704b0d1
bouchra <bouchraabdelmouktadir1@gmail.com@example.com>
2023-03-22 08:55:27 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,18 @@
-#include "main.h"
-
-/**
  * main - Entry point
- * Return: Always 0
+ *
+ * Description: prints _putchar using putchar prptptype
+ *
+ * Return: Always 0 (success)
  */
 
 int main(void)
-
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
 	_putchar('\n');
+
 	return (0);
 }
