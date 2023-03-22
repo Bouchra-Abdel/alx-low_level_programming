d9040c3e2b43ca6ed27535f9f758a16e5fe034c5
bouchra <bouchraabdelmouktadir1@gmail.com@example.com>
2023-03-22 09:24:40 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,8 +11,13 @@ int main(void)
 	char str[] = "_putchar";
 	int i=0;
 
-	for (i;i < str.stlen(); i++){
-	    putchar(str[i]);
-	}
+	    putchar("p");
+	    putchar("u");
+	    putchar("t");
+	    putchar("c");
+	    putchar("h");
+	    putchar("a");
+	    putchar("r");
+	    putchar("\n");
 	return (0);
 }
